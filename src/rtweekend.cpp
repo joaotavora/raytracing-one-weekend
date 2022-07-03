d5eab5d1ab585061ab6d23ee3aa1c6301228ac91
João Távora <joaotavora@gmail.com>
2022-07-03 14:43:32 +0100

    Done 6.1.  Pretty blue sphere

--- src/rtweekend.cpp
+++ src/rtweekend.cpp
@@ -27,33 +27,40 @@ namespace rtweekend {
     vec3 at(double t) const {return origin + direction * t;}
   };
 
-  /** Tell if ray R hits sphere at CENTER with radius RADIUS */
-  bool hit_sphere(const point& center, double radius, const ray& r) {
+  /** Compute T of A+BT where ray R hits sphere at CENTER of radius RADIUS */
+  double hit_sphere(const point& center, double radius, const ray& r) {
     //  x = -b +- sqrt(b^2 - 4ac)/2a
     vec3 oc = r.origin - center;                      // (A-C)
     auto a = glm::dot(r.direction, r.direction);      // (b.b)
     auto b = 2.0 * glm::dot(oc, r.direction);         // 2b(A-C)
     auto c = glm::dot(oc, oc) - radius*radius;        // (A-C)(A-C) - r^2
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant > 0) return (-b - std::sqrt(discriminant))/(2 * a);
+    return -1;
+  }
+
+  /** Compute T of Ray R where it hits infinite slanted wall Z units away */
+  double hit_background(double slant, const ray& r) {
+    vec3 unit_direction = glm::normalize(r.direction);
+    auto ycomp = unit_direction.y;
+    // t is between 0 and 1, proportional to y
+    return 0.5*(ycomp + slant);
   }
 
   /** Say color of ray R */
   color ray_color(const ray& r) {
-    if (hit_sphere(vec3{0,0,-1}, 0.3, r)) return color{1.0, 0, 0};
-    // I then did a standard graphics trick of scaling that to
-    // 0.0≤t≤1.0. When t=1.0 I want blue. When t=0.0 I want
-    // white. In between, I want a blend. This forms a “linear
-    // blend”, or “linear interpolation”, or “lerp” for short,
-    // between two things. A lerp is always of the form
-    //
-    // blendedValue=(1−t)⋅startValue+t⋅endValue,
-    //
-    // with t going from zero to one.
-      vec3 unit_direction = glm::normalize(r.direction);
-    auto ycomp = unit_direction.y;
-    // t is between 0 and 1, proportional to y
-    auto t = 0.5*(ycomp + 1.0);
+    constexpr point sphere_center{0,0,-1};
+    constexpr double sphere_radius{0.5};
+
+    double t{};  // hit point of our ray r.
+
+    t = hit_sphere(sphere_center, sphere_radius, r);
+    if (t > 0) {
+      auto normal = glm::normalize(r.at(t) - sphere_center);
+      return 0.5*(normal + vec3{1,1,1});
+    }
+
+    t = hit_background(+1.0, r);
     return
       // lower t, whiter image, higher t, bluer image centered 
       (1.0-t)*color{1.0, 1.0, 1.0} + t*color{0.5, 0.7, 1.0};
@@ -67,7 +74,7 @@ int main() {
   // Image
 
   constexpr auto aspect_ratio = 16.0/9.0;
-  constexpr int image_width = 400;
+  constexpr int image_width = 600;
   constexpr int image_height = static_cast<int>(image_width/aspect_ratio);
 
 
