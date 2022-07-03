02c4cb1c05c658f1d7cc524ff327abcd449aa4e8
João Távora <joaotavora@gmail.com>
2022-07-03 12:49:17 +0100

    Before 5. "Adding a sphere"

--- src/rtweekend.cpp
+++ src/rtweekend.cpp
@@ -1,12 +1,19 @@
+#include <cmath>
 #include <iostream>
 #include <glm/glm.hpp>
+#include <functional>
+
+
 
 namespace rtweekend {
   // could be "color.h"
-  using color = glm::vec3;
-  using point = glm::dvec3;
+  using vec3 = glm::dvec3;
+  using color = vec3;
+  using point = vec3;
 
   void write_color(std::ostream &out, color pixel_color) {
+
+
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x) << ' '
         << static_cast<int>(255.999 * pixel_color.y) << ' '
@@ -15,10 +22,29 @@ namespace rtweekend {
 
   // could be "ray.h"
   struct ray {
-    glm::dvec3 origin{};
-    glm::dvec3 direction{};
-    glm::dvec3 at(double t) const {return origin + direction * t;}
+    vec3 origin{};
+    vec3 direction{};
+    vec3 at(double t) const {return origin + direction * t;}
   };
+
+  color ray_color(const ray& r) {
+    // I then did a standard graphics trick of scaling that to
+    // 0.0≤t≤1.0. When t=1.0 I want blue. When t=0.0 I want
+    // white. In between, I want a blend. This forms a “linear
+    // blend”, or “linear interpolation”, or “lerp” for short,
+    // between two things. A lerp is always of the form
+    //
+    // blendedValue=(1−t)⋅startValue+t⋅endValue,
+    //
+    // with t going from zero to one.
+      vec3 unit_direction = glm::normalize(r.direction);
+    auto ycomp = unit_direction.y;
+    // t is between 0 and 1, proportional to y
+    auto t = 0.5*(ycomp + 1.0);
+    return
+      (1.0-t)*color(1.0, 1.0, 1.0) + // lower t, whiter image 
+      t*color(0.5, 0.7, 1.0); // higher t, bluer image
+  }
 }  // namespace rtweekend
 
 
@@ -27,13 +53,20 @@ int main() {
   namespace rt=rtweekend;
   // Image
 
-  const int image_width = 256;
-  const int image_height = 256;
+  constexpr auto aspect_ratio = 16.0/9.0;
+  constexpr int image_width = 400;
+  constexpr int image_height = static_cast<int>(image_width/aspect_ratio);
 
-  rt::ray r{
-    .origin    = {1,2,3},
-    .direction = {12,3,4},
-  };
+
+  auto viewport_height = 2.0;
+  auto viewport_width = aspect_ratio * viewport_height;
+  auto focal_length = 1.0;
+
+  auto origin = rt::vec3{0,0,0};
+  auto horizontal = rt::vec3{viewport_width, 0, 0};
+  auto vertical = rt::vec3{0, viewport_height, 0};
+  auto lower_left_corner =
+    origin - horizontal/2.0 - vertical/2.0 - rt::vec3{0, 0, focal_length};
 
   // Render
 
@@ -42,9 +75,14 @@ int main() {
   for (int j = image_height-1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      rt::color pixel_color{double(i)/(image_width-1),
-                            double(j)/(image_height-1),
-                            0.25};
+      auto u = double(i) / (image_width-1);   // sweep [0 -> 1]
+      auto v = double(j) / (image_height-1);  // sweep [1 -> 0]
+      rt::ray r{
+        .origin   = origin,
+        .direction= (lower_left_corner + u*horizontal + v*vertical) - origin
+      };
+      rt::color pixel_color = rt::ray_color(r);
+
       rt::write_color(std::cout, pixel_color);
     }
   }
